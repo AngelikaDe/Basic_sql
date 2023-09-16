85409c2c8d4cd0a4b11108538b8350e60bcaad64
Анжелика Демина <angelika@MacBook-Pro-Anzelika-3.local>
2023-09-16 19:09:58 +0300

    1st day

---
+++ day08.sql
@@ -0,0 +1,12 @@
+-- -- 1 session
+-- BEGIN;
+
+-- -- 1 session
+-- UPDATE pizzeria DET rating = 5 WHERE name = 'Pizza Hut';
+-- COMMIT;
+
+-- -- 2 session
+-- SELECT * FROM pizzeria;
+
+-- -- 2 session
+-- SELECT * FROM pizzeria;
